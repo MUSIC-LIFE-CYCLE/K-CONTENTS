f52bc98347e7380d4fa05aae0838c31f3923c494
Lee Dano <eksh7080@gmail.com>
2023-03-04 02:28:18 +0900

    ✨feat: 구글로그인 연결 완료

---
+++ front/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+import type { NextAuthOptions } from 'next-auth';
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    pages: {
+        signIn: '/auth/login',
+    },
+};
+export default NextAuth(authOptions);
